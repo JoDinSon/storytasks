22105f085fe19bf098080561270568a2d35b69f8
Joey6406 <72601033+Joey6406@users.noreply.github.com>
2024-08-28 11:30:20 +0200

    Initial commit

---
+++ src/test/java/dev/johoerl/storytasks/StorytasksApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.johoerl.storytasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorytasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
